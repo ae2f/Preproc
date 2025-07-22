db273b6e67eb245dd72d1df1a4b3acbd3969f214
ae2f <mincraft20046@gmail.com>
2025-07-22 11:38:39 +0900

    Add fileraw 	(Maybe I need AI for documentation)
     Changes to be committed:
    	modified:   CMakeLists.txt
    	new file:   FileRaw/CMakeLists.txt
    	new file:   FileRaw/main.c
    	new file:   cmake/FileRaw.cmake

---
+++ FileRaw/main.c
@@ -0,0 +1,69 @@
+#include "./main.auto.h"
+#include <stdio.h>
+
+#if STRGEN
+
+int Ch;
+char BackSlash[3] = "\\\\";
+
+int main() {
+	fputc('"', stdout);
+	while((Ch = fgetc(stdin)) != EOF) {
+		switch(Ch) {
+			case '\n': case '\r': /** Line Changing */
+				fputs("\\n\"\n\"", stdout);
+				break;
+			case '\\': case '\'': case '\"':
+				BackSlash[1] = Ch;
+				fputs(BackSlash, stdout);
+				break;
+			case '\t':
+				BackSlash[1] = 't';
+				fputs(BackSlash, stdout);
+				break;
+			default:
+				fputc(Ch, stdout);
+				break;
+		}
+	}
+
+	fputc('"', stdout);
+	return 0;
+}
+#else
+
+char	Buff[BINGEN_BUFFSZ];
+size_t	BuffLen, BuffI;
+
+int main() {
+	BuffLen = fread(Buff, 1, 1, stdin);
+	if(ferror(stdout)) {
+		return -1;
+	}
+	if(!BuffLen || feof(stdin)) {
+		printf("{}");
+		return 0;
+	}
+
+	printf("{%d", Buff[0]);
+
+RECURSE:
+
+	BuffLen = fread(Buff, 1, BINGEN_BUFFSZ, stdin);
+	if(ferror(stdin)) {
+		return -1;
+	}
+
+	if(BuffLen) {
+		for(BuffI = 0; BuffI < BuffLen; BuffI++) {
+			printf(",%d", Buff[BuffI]);
+		}
+
+		goto RECURSE;
+	}
+
+	putc('}', stdout);
+	return 0;
+}
+
+#endif
---
+++ cmake/FileRaw.cmake
@@ -0,0 +1,62 @@
+function(ae2f_FileRaw_init prm_STRGEN prm_BINGEN_BUFFSZ)
+	file(REMOVE_RECURSE ${ae2f_FileRaw_ROOT}/build)
+	message("[ae2f_FileRaw_init]  ${CMAKE_GENERATOR}")
+
+	if(DEFINED CMAKE_C_STANDARD)
+		set(cstd "-DCMAKE_C_STANDARD=${CMAKE_C_STANDARD}")
+	else()
+		set(cstd "")
+	endif()
+
+	if(DEFINED CMAKE_C_COMPILER)
+		set(cc "-DCMAKE_C_COMPILER=${CMAKE_C_COMPILER}")
+	else()
+		set(cc "")
+	endif()
+
+	if(DEFINED CMAKE_GENERATOR)
+		set(gen "-G${CMAKE_GENERATOR}")
+	else()
+		set(gen "")
+	endif()
+
+	execute_process(
+		WORKING_DIRECTORY ${ae2f_FileRaw_ROOT}
+		COMMAND ${CMAKE_COMMAND} 
+		"-S" "." "-B" "./build"
+		-DSTRGEN=${prm_STRGEN}
+		-DBINGEN_BUFFSZ=${prm_BINGEN_BUFFSZ}
+		${gen} ${cstd} ${cc}
+		${ARGN}
+  		RESULT_VARIABLE ConfOut
+	)
+
+ 	if(NOT ConfOut EQUAL 0)
+		message(FATAL_ERROR "[ae2f_FileRaw_init] Configuration failed. ${ConfOut}")
+	endif()
+
+	execute_process(
+		WORKING_DIRECTORY ${ae2f_Macro_ROOT}
+		COMMAND ${CMAKE_COMMAND} "--build" "build"
+  		RESULT_VARIABLE BuildOut
+		)
+  
+ 	if(NOT BuildOut EQUAL 0)
+		message(FATAL_ERROR "[ae2f_FileRaw_init] Build failed. ${BuildOut}")
+	endif()
+
+	message("[ae2f_FileRaw_init] Succeed.")
+endfunction()
+
+
+function(ae2f_FileRaw_Run_One inp_file_absolute out_file_absolute)
+	file(GLOB_RECURSE cmd ${ae2f_FileRaw_ROOT}/build/bin/**)
+	get_filename_component(dir "${inp_file_absolute}" DIRECTORY)
+
+	execute_process(
+		WORKING_DIRECTORY 
+		COMMAND		${cmd} ${dir} ${ARGN}
+		INPUT_FILE	${inp_file_absolute}
+		OUTPUT_FILE	${out_file_absolute}
+	)
+endfunction()
