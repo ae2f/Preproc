ad4aaf6a55565c540881461516862ae2226a171d
ae2f <mincraft20046@gmail.com>
2025-08-10 19:10:00 +0900

    Updated README and removed C++ test on test.sh which is not necessary.
     Changes to be committed:
    	modified:   README.md
    	modified:   test.sh

--- README.md
+++ README.md
@@ -1,12 +1,17 @@
 # ae2f::Preproc
-- Processes simple include preprocessor written in C.
+The cmake utilities for enabling template meta programming in C with macro-function generating and including.  
+Basically it uses `stdin` and `stdout` for processing, and you will need to pipeline those outputs.  
+    > Which means you will making new files.  
+
+It is written in standard ANSI C, which is the lowest version covered by cmake.  
+The sources now covers from ANSI C to C23.
 
 # ae2f_PreProc_C_COMPILER
 When you define this value on cmake, 't will be expected to valid C compiler for configuration step.
 
 # Requirements
-- A C compiler >= 98 (ANSI C)
+- A C compiler >= 90 (aka ANSI C)
 - cmake >= 3.20
-- A functional OS which supports standard library of C.
+- A functional OS which supports standard library of C and pipelining.
     - Unix based systems are recommended.
     - Windows is tested.
--- test.sh
+++ test.sh
@@ -6,7 +6,6 @@
 makers=("-GNinja" "")
 # buildtypes=("Release" "Debug" "MinSizeRel" "RelWithDebInfo")
 lstdc=("90" "99" "11" "17" "23")
-lstdcc=("14" "17" "20" "23")
 
 __ae2f_CXX=("ON" "OFF")
 __ae2f_IS_SHARED=("ON" "OFF")
@@ -14,16 +13,14 @@ __ae2f_IS_SHARED=("ON" "OFF")
 buildtype=$3
 
 for stdc in ${lstdc[@]}; do
-for stdcc in ${lstdcc[@]}; do
 for maker in ${makers[@]}; do
 for _ae2f_CXX in ${__ae2f_CXX[@]}; do
 for _ae2f_IS_SHARED in ${__ae2f_IS_SHARED[@]}; do
 
-builddir=build/B$1$2$buildtype$stdc$stdcc$makers$_ae2f_CXX$_ae2f_IS_SHARED
+builddir=build/B$1$2$buildtype$stdc$makers$_ae2f_CXX$_ae2f_IS_SHARED
 
 cmake -S . -B $builddir \
 	-DCMAKE_C_STANDARD=$stdc \
-	-DCMAKE_CXX_STANDARD=$stdcc \
 	$maker $1 $2 \
 	-Dae2f_CXX=$_ae2f_CXX \
 	-Dae2f_IS_SHARED=$_ae2f_IS_SHARED || { echo "Configuration failed"; exit 1; }
@@ -34,5 +31,4 @@ ctest --test-dir $builddir -C $buildtype || { echo "Test failed"; exit 1; }
 done
 done
 done
- done
  done
