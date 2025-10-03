7e9be2c08d815373593e80881b12f1d0ac5e483a
ae2f <mincraft20046@gmail.com>
2025-10-03 21:55:40 +0900

    "ae2f_Macro_Lib_V" Now uses full relative path
     Changes to be committed:
    	modified:   cmake/Macro.cmake

--- cmake/Macro.cmake
+++ cmake/Macro.cmake
@@ -7,14 +7,14 @@ function(ae2f_Macro_init prm_CMT_REQUIRED prm_SZPARAM prm_SZTPARAM)
 	file(REMOVE_RECURSE ${ae2f_Macro_ROOT}/build)
   	message("[ae2f_Macro_init]  ${CMAKE_GENERATOR}")
 
-	if(DEFINED CMAKE_C_STANDARD)
-		set(cstd "-DCMAKE_C_STANDARD=${CMAKE_C_STANDARD}")
+	if(DEFINED ae2f_PreProc_CMAKE_C_STANDARD)
+		set(cstd "-DCMAKE_C_STANDARD=${ae2f_PreProc_CMAKE_C_STANDARD}")
 	else()
 		set(cstd "")
 	endif()
 
-	if(DEFINED CMAKE_C_COMPILER)
-		set(cc "-DCMAKE_C_COMPILER=${CMAKE_C_COMPILER}")
+	if(DEFINED ae2f_PreProc_CMAKE_C_COMPILER)
+		set(cc "-DCMAKE_C_COMPILER=${ae2f_PreProc_CMAKE_C_COMPILER}")
 	else()
 		set(cc "")
 	endif()
@@ -108,9 +108,12 @@ function(ae2f_Macro_Lib_V prm_namespace prm_name prm_prefix
 )
 	file(GLOB_RECURSE files-inc "${prm_inc_dir}/${prm_inc_glob}")
 	file(GLOB_RECURSE files-src "${prm_src_dir}/${prm_src_glob}")
+	file(GLOB_RECURSE orig-inc  "${prm_include_dir}/*")
 
 	foreach(file ${files-inc})
-		ae2f_Macro_cvrt(${file} ${prm_inc_out_dir} ${prm_inc_out_ext})
+		get_filename_component(path_dir "${file}" DIRECTORY)
+		file(RELATIVE_PATH path_dir_rel ${prm_inc_dir} ${path_dir})
+		ae2f_Macro_cvrt(${file} ${prm_inc_out_dir}/${path_dir_rel} ${prm_inc_out_ext})
 	endforeach()
 
 	file(GLOB_RECURSE ofiles-inc "${prm_inc_out_dir}/*${prm_inc_out_ext}")
@@ -119,15 +122,15 @@ function(ae2f_Macro_Lib_V prm_namespace prm_name prm_prefix
 		ae2f_CoreLibTentConfigCustom(
 			${prm_name} ${prm_prefix} ${prm_include_dir} 
 			${prm_namespace} ${prm_config_file}
-			${ARGN} ${files-src} ${ofiles-inc}
-		)
+			${ARGN} ${files-src} ${ofiles-inc} ${orig-inc}
+			)
 	else()
 		ae2f_CoreLibTentConfigCustom(
 			${prm_name} INTERFACE ${prm_include_dir} 
 			${prm_namespace} ${prm_config_file} 
 			${ofiles-inc}
-			${ARGN}
-		)
+			${ARGN} ${orig-inc}
+			)
 	endif()
 endfunction()
 
@@ -135,13 +138,13 @@ function(ae2f_Macro_Lib prm_namespace prm_name prm_prefix
 		prm_in_dir prm_in_glob 
 		prm_out_dir prm_out_ext 
 		prm_config_file prm_include_dir
-	)
+		)
 	ae2f_Macro_Lib_V(
 		${prm_namespace} ${prm_name} ${prm_prefix} 
 		${prm_in_dir} ${prm_in_glob} 
 		${prm_in_dir} ${prm_in_glob} ${prm_out_dir} ${prm_out_ext}
 		${prm_config_file} ${prm_include_dir}
-	)
+		)
 endfunction()
 
 function(ae2f_Macro_autoname prm_in)
